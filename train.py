b686a624264dfafc3e98d041c7a4e4575a15c0dc
Paul Ewins <2139206e@student.gla.ac.uk>
2023-01-26 11:51:26 +0000

    print statements for debugging

--- train.py
+++ train.py
@@ -28,6 +28,8 @@ if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)    # get the number of images in the dataset.
+    print(dataset)
+    print(dataset_size)
     print('The number of training images = %d' % dataset_size)
 
     model = create_model(opt)      # create a model given opt.model and other options
