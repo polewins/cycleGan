a108a84afeb0bc37f1885b70bcab0b55c83ee111
Paul Ewins <2139206e@student.gla.ac.uk>
2023-01-28 10:46:31 +0000

    resize data in unaligned_dataset.py

--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -65,8 +65,9 @@ class UnalignedDataset(BaseDataset):
         A = torch.from_numpy(A_img)
         B = torch.from_numpy(B_img)
         
-        print(A)
-        print(B)
+        np.resize(A,(256, 256))
+        np.resize(B,(256, 256))
+        
         return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
 
     def __len__(self):
