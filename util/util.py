b3799f4060bba95038b47cec1d38b563958a3880
Paul Ewins <2139206e@student.gla.ac.uk>
2023-02-21 18:29:29 +0000

    changed to save output as npy

--- util/util.py
+++ util/util.py
@@ -61,6 +61,7 @@ def save_image(image_numpy, image_path, aspect_ratio=1.0):
         image_pil = image_pil.resize((h, int(w * aspect_ratio)), Image.BICUBIC)
     if aspect_ratio < 1.0:
         image_pil = image_pil.resize((int(h / aspect_ratio), w), Image.BICUBIC)
+    print(image_path)
     image_path.replace("png", "npy")
     image_pil.save(image_path)
 
